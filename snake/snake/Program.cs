e78309440816d17f4500ce522ce4c7a19f15a250
mikhail_elizarov <4309030@gmail.com>
2022-03-09 23:35:17 +0300

    class point

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -1,22 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 {
-    int x1 = 1;
-    int y1 = 3;
-    char sym1 = '*';
+    Point p1 = new Point();
+    p1.x = 1;
+    p1.y = 3;
+    p1.sym = '*';
+    p1.Draw();
 
-    Draw(x1, y1, sym1);
+    Point p2 = new Point();
+    p2.x = 1;
+    p2.y = 3;
+    p2.sym = '#';
+    p1.Draw();
 
-    int x2 = 4;
-    int y2 = 5;
-    char sym2 = '#';
-
-    Draw(x2, y2, sym2);
+   
 
     Console.ReadLine();
-}
-
-static void Draw(int x, int y, char sym)
-{
-    Console.SetCursorPosition(x, y);
-    Console.Write(sym);
 }
