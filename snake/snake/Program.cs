34a03244a44584a0e935970604269304e914f39b
mike_elizarov <gecko84@yandex.ru>
2022-03-10 16:56:36 +0300

    constructor

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -1,17 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 {
-    Point p1 = new Point();
-    p1.x = 1;
-    p1.y = 3;
-    p1.sym = '*';
+    Point p1 = new Point(1, 3, '*');
+   
     p1.Draw();
 
-    Point p2 = new Point();
-    p2.x = 1;
-    p2.y = 3;
-    p2.sym = '#';
-    p1.Draw();
+    Point p2 = new Point(4, 5, '#');
+    
+    p2.Draw();
 
    
 
