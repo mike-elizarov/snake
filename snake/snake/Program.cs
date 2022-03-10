7afecfc9466da35d01f2a35d505e744ad911f6db
mike_elizarov <gecko84@yandex.ru>
2022-03-10 17:54:46 +0300

    list

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -9,7 +9,46 @@
     
     p2.Draw();
 
-   
+    Point p3 = new Point(6, 7, '*');
+
+    p3.Draw();
+
+    Point p4 = new Point(8, 9, '#');
+
+    p4.Draw();
+
+    Point p5 = new Point(10, 11, '*');
+    
+    p5.Draw();
+
+    Point p6 = new Point(12, 13, '#');
+
+    p6.Draw();
+
+    List<int> charlist = new List<char>();
+    charlist.Add('*');
+    charlist.Add('#');
+    charlist.Add('*');
+    charlist.Add('#');
+    charlist.Add('*');
+    charlist.Add('#');
+
+
+    
+    /*foreach(int i in numlist)
+    {
+        Console.WriteLine(i);
+    }*/
+
+    List<Point> plist = new List<Point>();
+    plist.Add(p1);
+    plist.Add(p2);
+    plist.Add(p3);
+    plist.Add(p4); 
+    plist.Add(p5);
+    plist.Add(p6);
+
+
 
     Console.ReadLine();
 }
