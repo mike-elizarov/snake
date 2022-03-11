604edc3d3bb26aafa6921707dfe150d52bd7c3bc
mike_elizarov <gecko84@yandex.ru>
2022-03-12 00:07:39 +0300

    mid

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -1,21 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 {
-    //Console.SetBufferSize(80, 25);
+   // Console.SetBufferSize(80, 25);
 
     HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
-    /*HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+    HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
     VerticalLine leftlineline = new VerticalLine(0, 24, 0, '+');
-    VerticalLine rightlineline = new VerticalLine(0, 24, 78, '+');*/
-    upline.Drow;
-    /*downline.Drow;
-    leftlineline.Drow;
-    rightlineline.Drow;
+    VerticalLine rightlineline = new VerticalLine(0, 24, 78, '+');
+    upline.Drow();
+    downline.Drow();
+    leftlineline.Drow();
+    rightlineline.Drow();
 
 
     //Отрисовка точек
     Point p = new Point(4, 5, '*');
-    p.Draw;*/
+    p.Draw();
 
     Console.ReadLine();
 }
